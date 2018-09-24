64d5a7486bd7b7783552568e6a429879423fbeb3
sspboyd <sspboyd@gmail.com>
2018-09-23 22:34:33 -0400

    This has gone surprisingly better than I expected. Still no idea if this is a sane way of organizing functions and variables but its working and some of it even looks 'functional'-ish. My earlier stumbling block was that I didn't realize I'd muted the tab in Chrome, so no matter what I did, I wasn't going to hear anything. Live and learn.

--- sketch.js
+++ sketch.js
@@ -1,59 +1,53 @@
-// var synth1 = new Synth();
-var osc = new p5.Oscillator();
-
-// function Synth() {
-//     //this.osc = new p5.Oscillator();
-
-//     this.setupCall = function() {
-//         //call during setup
-//         console.log("setupCall this", this);
-//         // this.slider = createSlider(100, 1200, 440);
-//         // this.button = createButton("on/off");
-//         // this.button.mousePressed(this.toggle);
-//         // this.playing = true;
-//     }
-// }
-
-// function toggle(synth) {
-//     console.log(synth);
-//     if (!synth.osc.playing) {
-//         synth.osc.playing = true;
-//         synth.osc.start();
-//         synth.osc.amp(.5, 1);
-//         synth.osc.freq(440);
-//         console.log("playing");
-//     } else {
-//         playing = false;
-//         synth.osc.stop();
-//         synth.osc.amp(0, 1);
-//         console.log("stopped");
-//     }
-// }
-
-
+var synths = [];
 
 function setup() {
-    createCanvas(100, 100);
-    background(47);
-    //synth1.setupCall();
-    // synth1.slider = createSlider(100, 1200, 440);
-    // synth1.button = createButton("on/off");
-    // synth1.button.mousePressed(function() { toggle(synth1); });
-    // synth1.playing = true;
-    // synth1.osc = new p5.Oscillator();
-
-    // synth1.osc.start();
-    // synth1.osc.setType("triangle");
-    // synth1.osc.amp(1);
-    // synth1.osc.freq(440);
-
-    osc.start();
-    osc.setType("triangle");
-    osc.amp(.3);
-    osc.freq(440);
+    var newSyn = {};
+    newSyn.osc = synth.initOsc();
+    newSyn.ui = synth.initUI(newSyn.osc);
+    synths.push(newSyn);
 }
 
 function draw() {
-    // var newFreq = synth1.slider.value();
-    // synth1.osc.freq(newFreq);
+    background(47);
+
+    synths.forEach(function(e, i, a) {
+        var newFreq = e.ui.freqSlider.value();
+        var newVol = e.ui.volSlider.value();
+        e.osc.amp(newVol);
+        e.osc.freq(newFreq);
+    })
 }
+
+var synth = {
+    initOsc: function() {
+        var playing = false;
+        var synOsc = new p5.Oscillator();
+        synOsc.start();
+        synOsc.amp(0.5, .5);
+        synOsc.setType("square");
+        synOsc.freq(300);
+        return synOsc;
+    },
+    initUI: function(osc) {
+        var onOffBtn = createButton("syn on/off");
+        onOffBtn.mousePressed(function() {
+            if (!osc.started) {
+                osc.start();
+                // osc.amp(.5, .4);
+                console.log("playing");
+            } else {
+                osc.stop(1);
+                osc.amp(0, .8);
+                console.log("not playing");
+            }
+        });
+        var freqSldr = createSlider(50, 2000, 440);
+        var volSldr = createSlider(0.0, 1.0, 0.4, 0.025);
+        var ui = {
+            onOffButton: onOffBtn,
+            freqSlider: freqSldr,
+            volSlider: volSldr
+        };
+        return ui;
+    }
+};
